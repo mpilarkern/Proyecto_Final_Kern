2f393373140467ebde5812c48fc4aa7222532a3d
Pilar Kern <pilikern04@gmail.com>
2023-11-18 17:19:28 -0300

    agrego nuevas funciones

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import list_articles, post_article, search_articles
-
+from blog.views import search_articles, ArticleListView, ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 urlpatterns = [
-    path('articles/', list_articles, name='articles_list'),
-    path('new-article/', post_article, name='new_article'),
     path('search-articles/', search_articles, name='search_articles'),
+    path('articles/', ArticleListView.as_view(), name='articles_list'),
+    path("articles/<int:pk>/", ArticleDetailView.as_view(), name="read_article"),
+    path('new-article/', ArticleCreateView.as_view(), name='new_article'),
+    path('edit-article/<int:pk>/', ArticleUpdateView.as_view(), name='edit_article'),
+    path('delete-article/<int:pk>/', ArticleDeleteView.as_view(), name='delete_article'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,43 +1,9 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.db.models import Q
 from blog.models import Article
 from blog.forms import ArticleForm
-
-def list_articles(request):
-    contexto = {'articles': Article.objects.all()}
-    http_response = render(
-        request=request,
-        template_name='blog/articles_list.html',
-        context=contexto,
-    )
-    return http_response
-
-def post_article(request):
-    if request.method == "POST":
- 
-        formulario = ArticleForm(request.POST) 
- 
-        if formulario.is_valid():
-                  data=formulario.cleaned_data
-                  title=data["title"]
-                  subtitle=data['subtitle']
-                  body=data['body']
-                  author=data['author']
-                  date=data['date']
-                  article = Article(title=title, subtitle=subtitle, body=body, author=author, date=date)
-                  article.save()
-                  url_exitosa = reverse('articles_list')  
-                  return redirect(url_exitosa)
-    else:
-        formulario = ArticleForm()
-    
-    http_response = render(
-        request=request,
-        template_name='blog/new_article.html',
-        context={'formulario': formulario}
-    )
-    return http_response
 
 def search_articles(request):
     if request.method == "POST":
@@ -45,7 +11,7 @@ def search_articles(request):
         busqueda = data["search"]
         
         articles = Article.objects.filter(
-            Q(title__icontains=busqueda)
+            Q(title__icontains=busqueda) | Q(author__icontains=busqueda) 
         )
 
         contexto = {
@@ -57,3 +23,25 @@ def search_articles(request):
             context=contexto,
         )
         return http_response
+
+class ArticleListView(ListView):
+     model = Article
+     template_name = 'blog/articles_list.html'
+
+class ArticleCreateView(CreateView):
+     model = Article
+     fields = ('title', 'subtitle', 'body', 'author', 'date')
+     success_url = reverse_lazy('articles_list')
+
+class ArticleUpdateView(UpdateView):
+     model = Article
+     fields = ('title', 'subtitle', 'body', 'author', 'date')
+     success_url = reverse_lazy('articles_list')
+class ArticleDetailView(DetailView):
+    model = Article
+    success_url = reverse_lazy('articles_list')
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    success_url = reverse_lazy('articles_list')
+
