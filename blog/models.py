029ee9089f2ba40f3842ad4707a9e5861b71fdd5
Pilar Kern <pilikern04@gmail.com>
2023-11-18 09:52:25 -0300

    modelo articulo creado

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=64)
+    subtitle = models.CharField(max_length=256)
+    body = models.TextField(blank=True)
+    author = models.CharField(max_length=256)
+    date = models.DateField(null=True)
