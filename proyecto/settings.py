8424ec7d624ccb8a7433687dcb7176dbd66a31a9
Pilar Kern <pilikern04@gmail.com>
2023-11-19 20:11:58 -0300

    agrego avatar

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +134,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from proyecto.views import iniciar, about
 
 
@@ -26,3 +30,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('perfiles/', include('perfiles.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
