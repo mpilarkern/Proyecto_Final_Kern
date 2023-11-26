a1a66ec6f45b5b867c6ec3a892cfdda17bdb53ac
Pilar Kern <pilikern04@gmail.com>
2023-11-26 11:59:48 -0300

    solución de errores

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Article(models.Model):
     title = models.CharField(max_length=64)
     subtitle = models.CharField(max_length=256, blank=True)
-    body = models.TextField(blank=True)
+    body = models.TextField()
     date = models.DateField(auto_now=True, null=True)
     creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
